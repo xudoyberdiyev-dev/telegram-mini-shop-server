a0b8d180f963514cd61c6c3d4bc5e3943b3a79aa
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-06-28 00:24:08 +0500

    ss

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controller = require('../controller/productController');
 const upload = require('../middleware/multer');
 
-router.post('/', upload.single('image'), controller.createProduct);
+router.post('/', upload.single('file'), controller.createProduct);
 router.get('/', controller.getAllProducts);
 router.get('/:id', controller.getProductById);
-router.put('/:id', upload.single('image'), controller.updateProduct);
+router.put('/:id', upload.single('file'), controller.updateProduct);
 router.delete('/:id', controller.deleteProduct);
 
 module.exports = router;
