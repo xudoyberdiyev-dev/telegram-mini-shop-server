b7288a5931bb46d8e0c07e3fa2637f8c388e86bd
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-07-12 16:17:57 +0500

    v1

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -16,6 +16,12 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
 
 module.exports = upload;
