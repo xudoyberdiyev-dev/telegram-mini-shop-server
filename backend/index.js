f5a2f87f7d7c55ea0bbfdf2cfa49c6a5f66c7d96
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-07-14 21:54:15 +0500

    100

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
+
 // MongoDB ulash
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("✅ MongoDB ulandi"))
