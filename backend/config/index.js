f8ad89e2ed763510fd067ce6e1b12ceab0a43022
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-07-28 22:49:58 +0500

    test

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,2 +1,1 @@
 export const BASE_URL = "https://telegram-mini-shop-client.vercel.app/"
-// export const BASE_URL = "http://localhost:3000/"
--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -34,25 +34,3 @@ module.exports = multer({
     },
 });
 
-// const multer = require('multer');
-// const path = require('path');
-
-// // Har bir fayl nomi noyob bo'lishi uchun vaqt qo‘shiladi
-// const storage = multer.diskStorage({
-//     filename: function (req, file, cb) {
-//         cb(null, Date.now() + path.extname(file.originalname)); // masalan: 1719563782.png
-//     }
-// });
-
-// const fileFilter = (req, file, cb) => {
-//     const ext = path.extname(file.originalname).toLowerCase();
-//     if (ext === ".jpg" || ext === ".jpeg" || ext === ".png" || ext === ".webp") {
-//         cb(null, true);
-//     } else {
-//         cb(new Error("Faqat rasm fayllari yuklanadi!"));
-//     }
-// };
-
-// const upload = multer({ storage, fileFilter });
-
-// module.exports = upload;
